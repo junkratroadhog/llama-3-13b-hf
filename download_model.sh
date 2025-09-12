1c4abadf3b5c0e91e92e1377e36f79559f9f5793
junkratroadhog <junkratroadhogana@gmail.com>
2025-09-12 20:47:55 +0530

    added model

--- download_model.sh
+++ download_model.sh
@@ -6,15 +6,9 @@ HF_TOKEN="${HF_TOKEN:?HF_TOKEN must be set}"
 
 git lfs install
 
-# Configure Git to use Hugging Face token for HTTPS
-git config --global credential.helper store
-export GIT_ASKPASS=echo
-export GIT_USERNAME=hf
-export GIT_PASSWORD=$HF_TOKEN
-
 if [ ! -d "$MODEL_PATH/.git" ]; then
     echo "Cloning LLaMA repository with token..."
-    git clone https://huggingface.co/meta-llama/Llama-3-13b-hf "$MODEL_PATH"
+    git clone https://hf:${HF_TOKEN}@huggingface.co/meta-llama/Llama-3-13b-hf "$MODEL_PATH"
 fi
 
 cd "$MODEL_PATH"
